57bcf952332f2b8eb59126b0453cc9992c50d493
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-20 16:25:28 +0100

    Add files via upload

---
+++ controllercondocumentazioneinswagger/src/main/java/com/controllercondocumentazioneinswagger/controllercondocumentazioneinswagger/ControllercondocumentazioneinswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.controllercondocumentazioneinswagger.controllercondocumentazioneinswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllercondocumentazioneinswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllercondocumentazioneinswaggerApplication.class, args);
+	}
+
+}
---
+++ controllercondocumentazioneinswagger/src/test/java/com/controllercondocumentazioneinswagger/controllercondocumentazioneinswagger/ControllercondocumentazioneinswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controllercondocumentazioneinswagger.controllercondocumentazioneinswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllercondocumentazioneinswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
